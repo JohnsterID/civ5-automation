ab626c5587059defd5cf6153758f7cb6f86a34c5
JohnsterID <69278611+JohnsterID@users.noreply.github.com>
2023-04-28 22:49:47 +0800

    Update auto_build_and_deploy.py

--- auto_build_and_deploy.py
+++ auto_build_and_deploy.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import shutil
 import time
+import psutil
 
 # Specify the Git repository URL and local folder path
 git_repo_url = "https://github.com/LoneGazebo/Community-Patch-DLL.git"
@@ -16,8 +17,27 @@ dest_folder_path1 = os.path.expanduser(r"~\Documents\My Games\Sid Meier's Civili
 
 while True:
     # Check for updates in the Git repository
-    subprocess.check_call(["git", "clone", git_repo_url]) if not os.path.isdir(git_repo_path) else None
-    subprocess.check_call(["git", "pull"], cwd=git_repo_path)
+    if os.path.exists(git_repo_path):
+        # Get the Git repository head commit hash
+        output = subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=git_repo_path)
+        current_commit_hash = output.strip().decode("utf-8")
+
+        # Fetch the latest changes from the Git repository
+        subprocess.check_call(["git", "fetch"], cwd=git_repo_path)
+
+        # Check if there are any changes to the Git repository
+        output = subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=git_repo_path)
+        latest_commit_hash = output.strip().decode("utf-8")
+        if latest_commit_hash != current_commit_hash:
+			FIXME: See other comment. Need graceful stop of autoplay.
+            print("Stopping autoplay.py due to Git update.")
+            # Find the CivilizationV.exe process and terminate it
+            for process in psutil.process_iter():
+                if process.name() == "CivilizationV.exe":
+                    process.terminate()
+            break
+    else:
+        subprocess.check_call(["git", "clone", git_repo_url])
 
     # Run the build script
     subprocess.check_call(["python", build_script_path, "--config", "debug"], cwd=git_repo_path)
@@ -29,11 +49,25 @@ while True:
         shutil.copy(src_file_path, dest_file_path)
 
     # Remove CvGameCore_Expansion2.dll and LUA folder from the MOD folder
-    os.remove(os.path.join(dest_folder_path1, "CvGameCore_Expansion2.dll"))
-    shutil.rmtree(os.path.join(dest_folder_path1, "LUA"))
+    if os.path.exists(os.path.join(dest_folder_path1, "CvGameCore_Expansion2.dll")):
+        os.remove(os.path.join(dest_folder_path1, "CvGameCore_Expansion2.dll"))
+    if os.path.exists(os.path.join(dest_folder_path1, "LUA")):
+        shutil.rmtree(os.path.join(dest_folder_path1, "LUA"))
 
+    # FIXME: Add a way to gracefully stop autoplay.py if there is a git update before building.
+    #        Also, check if CivilizationV.exe is still running from the autoplay script before trying to run it again.
+    #        Ideally, CivilizationV.exe is stopped with the graceful stop of autoplay.py as we don't want it running when we build either as we need the resources.
     # Run the autoplay script
-    subprocess.check_call(["python", "autoplay.py"], cwd=dest_folder_path1)
+    while True:
+        # Check if CivilizationV.exe is still running from the previous autoplay session
+        for process in psutil.process_iter():
+            if process.name() == "CivilizationV.exe" and process.cwd() == dest_folder_path1:
+                print("Waiting for CivilizationV.exe to exit before starting autoplay.py.")
+                time.sleep(60)
+                break
+        else:
+            subprocess.check_call(["python", "autoplay.py"], cwd=dest_folder_path1)
+            break
 
     # Wait for 1 hour before checking for updates again
     time.sleep(3600)
